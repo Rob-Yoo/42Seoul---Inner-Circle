0357d00266f9da123ed3a715787044eafff0e09c
Jin young <jinyoo@c8r2s12.42seoul.kr>
2021-05-21 22:22:13 +0900

    gnl

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/21 16:31:48 by jinyoo            #+#    #+#             */
+/*   Updated: 2021/05/21 16:49:24 by jinyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GNL_H
+# define GNL_H
+# include <stdilb.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5000
+#endif
+
+#endif
