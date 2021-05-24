fdb9233349a58b8b4f914b8b4cdd453b7d8618dd
Jin young <jinyoo@c8r2s12.42seoul.kr>
2021-05-24 13:04:20 +0900

    gnl

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 16:31:48 by jinyoo            #+#    #+#             */
-/*   Updated: 2021/05/21 16:49:24 by jinyoo           ###   ########.fr       */
+/*   Updated: 2021/05/24 13:03:58 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # endif
 
 #ifndef OPEN_MAX
-# define OPEN_MAX 32
+# define OPEN_MAX 256
 #endif 
 
 int     get_next_line(int fd, char **line);
--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 23:23:24 by jinyoo            #+#    #+#             */
-/*   Updated: 2021/05/23 23:23:26 by jinyoo           ###   ########.fr       */
+/*   Updated: 2021/05/24 13:03:22 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # endif
 
 #ifndef OPEN_MAX
-# define OPEN_MAX 32
+# define OPEN_MAX 256
 #endif 
 
 int     get_next_line(int fd, char **line);
