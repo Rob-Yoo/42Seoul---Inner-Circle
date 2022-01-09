ae5e0eba56ea13a47d4aaf9a91bc50ccb9b8a5af
Rob-Yoo <jin12243@naver.com>
2022-01-09 16:02:49 +0900

    asdf

--- minitalk/include/client.h
+++ minitalk/include/client.h
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 16:27:08 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/01/09 15:48:06 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/01/07 16:32:25 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <sys/types.h>
 # include <signal.h>
+# include <stdlib.h>
 # include "util.h"
 
 typedef struct s_msg
