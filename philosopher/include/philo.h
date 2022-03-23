4231c405ebceddb41a1be7fa57e12028f16bd2e0
Rob-Yoo <jin12243@naver.com>
2022-03-23 17:10:57 +0900

    Feat : Heare File

---
+++ philosopher/include/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 14:36:49 by jinyoo            #+#    #+#             */
+/*   Updated: 2022/03/23 14:44:11 by jinyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <string.h>
+# define SLEEPING 1
+# define THINKING 2
+# define EATING 3
+
+#endif
