0ffc4cdb32b9af29473e17e107f4260db64dad87
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-03-30 11:17:09 +0900

    Docs : Header File

--- philosopher/include/philo.h
+++ philosopher/include/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 17:14:21 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/03/24 16:59:37 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/03/29 20:27:49 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@
 # include <stdio.h>
 # include <pthread.h>
 
-# define SLEEPING 1
-# define THINKING 2
-# define EATING 3
+# define PICKUP 1
+# define SLEEPING 2
+# define THINKING 3
+# define EATING 4
+# define FULL 5
+# define DEAD 6
+
+# define TRUE 1
+
+typedef pthread_mutex_t mutex;
 
 typedef struct s_inform
 {
@@ -29,16 +36,30 @@ typedef struct s_inform
 	int				timeToSleep;
 	int				numOfMustEat;
 	int				*state;
-	pthread_mutex_t	mutex;
+	mutex			main_lock;
+	mutex			*fork_mutex;
+	mutex			print_lock;
 }	t_inform;
 
 typedef struct s_phil
 {
-	t_inform	inform;
-	int			whoAmI;
+	t_inform	*inform;
+	pthread_t	tid;
+	int			me;
+	int			left_fork;
+	int			right_fork;
+	int			numOfEat;
 }	t_phil;
 
 int		ft_atoi(const char *str);
-void	*philosophers(void *inform);
+void	ft_putnbr(int n);
+void	ft_putstr(char *s);
 
+void	*dining_phils(void *inform);
+int		throw_error(t_phil *phils);
+int		print_state(t_phil *philo, int state);
+
+int		eat(t_phil *philo);
+// int		leftOf(t_phil *philo);
+// int		rightOf(t_phil *philo);
 #endif
