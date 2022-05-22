2af8d3937064cbc0de6520d1ced79bf15fa12416
Rob-Yoo <jin12243@naver.com>
2022-05-22 16:20:44 +0900

    Feat : ex00

---
+++ CPP_Module/CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 17:21:17 by jinyoo            #+#    #+#             */
+/*   Updated: 2022/05/22 16:10:27 by jinyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
