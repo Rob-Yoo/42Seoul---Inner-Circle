7f60219763ee91873e6201b3a8e59cf769981f4c
Rob-Yoo <jin12243@naver.com>
2022-06-17 22:11:14 +0900

    Feat : ex01

---
+++ CPP_Module/CPP_03/ex01/ScavTrap.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 17:28:04 by jinyoo            #+#    #+#             */
+/*   Updated: 2022/06/17 20:51:38 by jinyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAV_TRAP_HPP
+# define SCAV_TRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ScavTrap: ClapTrap() {
+	
+}
+
+#endif
