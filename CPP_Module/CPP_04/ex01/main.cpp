f0c4ea54f6eba57c3cab52d4604d538c3c819ba6
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-06-23 19:45:04 +0900

    Fix : ex01

--- CPP_Module/CPP_04/ex01/main.cpp
+++ CPP_Module/CPP_04/ex01/main.cpp
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 16:31:37 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/06/22 21:57:00 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/06/23 14:05:54 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main(void)
 {
@@ -20,14 +21,14 @@ int main(void)
 
 	std::cout << std::endl;
 
-	delete j;//should not create a leak
+	delete j;
 	delete i;
 
 	std::cout << std::endl;
 
 	Animal	*a[4];
 
-	for(int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++) {
 		if (i < 2) {
 			a[i] = new Cat();
 		}
@@ -38,45 +39,53 @@ int main(void)
 
 	std::cout << std::endl;
 
-	for(int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++) {
 		a[i]->makeSound();
 	}
 	
 	std::cout << std::endl;
 
-	for(int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++) {
 		delete a[i];
 	}
 
 	std::cout << std::endl;
 
-	{
-		// Cat	cat_1;
-		// Cat cat_2(cat_1);
-		Dog	dog_1;
-		Dog dog_2(dog_1);
+	WrongAnimal		*p = new WrongCat[4];
 
-		std::cout << std::endl;
+	std::cout << std::endl;
 
-		// for (int i = 0; i < 100; i++)
-		// 	std::cout << cat_2.getBrain()->getIdeas()[i] << std::endl;
-
-		// std::cout << std::endl;
-
-		// for (int i = 0; i < 100; i++)
-		// 	std::cout << cat_1.getBrain()->getIdeas()[i] << std::endl;
-
-		// std::cout << std::endl;
-		for (int i = 0; i < 100; i++)
-			std::cout << dog_2.getBrain()->getIdeas()[i] << std::endl;
-
-		std::cout << std::endl;
-
-		for (int i = 0; i < 100; i++)
-			std::cout << dog_1.getBrain()->getIdeas()[i] << std::endl;
-
-		std::cout << std::endl;
+	for (int i = 0; i < 4; i++) {
+		p[i].makeSound();
 	}
 
+	std::cout << std::endl;
+{
+	// Cat	cat_1;
+	// Cat cat_2(cat_1);
+	Dog	dog_1;
+	Dog dog_2(dog_1);
+
+	std::cout << std::endl;
+
+	// for (int i = 0; i < 100; i++)
+	// 	std::cout << cat_2.getBrain()->getIdeas()[i] << std::endl;
+
+	// std::cout << std::endl;
+
+	// for (int i = 0; i < 100; i++)
+	// 	std::cout << cat_1.getBrain()->getIdeas()[i] << std::endl;
+
+	// std::cout << std::endl;
+	for (int i = 0; i < 100; i++)
+		std::cout << dog_2.getBrain()->getIdeas()[i] << std::endl;
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < 100; i++)
+		std::cout << dog_1.getBrain()->getIdeas()[i] << std::endl;
+
+	std::cout << std::endl;
+}
 	return 0;
 }
