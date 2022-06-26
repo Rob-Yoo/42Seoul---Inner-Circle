de1f1fa74269abc0b7238b0e4007e434ed59b6c4
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-06-26 20:56:33 +0900

    Feat : ex01

---
+++ CPP_Module/CPP_05/ex01/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinyoo <jinyoo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/26 20:21:26 by jinyoo            #+#    #+#             */
+/*   Updated: 2022/06/26 20:55:49 by jinyoo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+int main()
+{
+	try
+	{
+		Bureaucrat a("jinyoo", 6);
+		Form f1("Report", 5, 5);
+		// Form f1("Report", 151, 5);
+
+		std::cout << std::endl;
+
+		//표준출력
+		std::cout << a << std::endl;
+		std::cout << f1 << std::endl;
+
+		a.signForm(f1);
+		std::cout << std::endl;
+
+		a.incre_grade(1);
+		a.signForm(f1);
+		std::cout << std::endl;
+
+		//표준출력
+		std::cout << a << std::endl;
+		std::cout << f1 << std::endl;
+		std::cout << std::endl;
+
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Exception : " << e.what() << std::endl;
+	}
+	return 0;
+}
