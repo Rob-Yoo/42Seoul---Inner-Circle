1e5549e74c9372554845d57d26269af170b7eb41
Rob-Yoo <jin12243@naver.com>
2022-06-28 16:20:40 +0900

    Fix : ex00

--- CPP_Module/CPP_05/ex00/main.cpp
+++ CPP_Module/CPP_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 20:50:16 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/06/27 17:31:41 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/06/28 16:20:21 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,22 @@ int	main(void)
 	try
 	{
 		Bureaucrat A("jinyoo", 10);
-		// 유효하지 않은 grade로 생성자 호출
-		Bureaucrat B(A);
-		Bureaucrat C("soo", 150);
+		Bureaucrat B("soo", 150);
 
 		std::cout << std::endl;
 
-		//표준출력
 		std::cout << A << std::endl;
 		std::cout << B << std::endl;
-		std::cout << C << std::endl;
 		std::cout << std::endl;
-
-		// A.incre_grade(11);
-		// A.incre_grade(10);
-		// A.decre_grade(141);
 
 		A.incre_grade(9);
-		// A.decre_grade(-9);
+		// A.incre_grade(10);
+		// B.decre_grade(1);
 
 		std::cout << std::endl;
 
-		//표준출력
 		std::cout << A << std::endl;
 		std::cout << B << std::endl;
-		std::cout << C << std::endl;
 		std::cout << std::endl;
 
 	}
