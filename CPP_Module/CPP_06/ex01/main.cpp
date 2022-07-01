32e5a1b6e4eff113208f0ad9f02b9cd418b5046b
Rob-Yoo <jin12243@naver.com>
2022-07-01 16:50:52 +0900

    Fix : ex01

--- CPP_Module/CPP_06/ex01/main.cpp
+++ CPP_Module/CPP_06/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jinyoo <jinyoo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 20:25:59 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/07/01 02:46:27 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/07/01 16:44:18 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int main(void)
 {
-	Data *data = new Data("jinyoo", 185.1, 72.6, 25);
-	std::cout << *data << std::endl;
+	Data *boxing_data = new Data("jinyoo", 185.1, 72.6, 25);
+	std::cout << *boxing_data << std::endl;
 
-	uintptr_t sr_data = serialize(data);
+	uintptr_t data_stream = serialize(boxing_data);
 
-	Data *dsr_data = deserialize(sr_data);
-	std::cout << *dsr_data << std::endl;
-	delete data;
+	Data *unboxing_data = deserialize(data_stream);
+	std::cout << *unboxing_data << std::endl;
+	delete boxing_data;
 	return 0;
 }
