eb79d775ec9c120c0777cc34e786eaea78a72807
Rob-Yoo <jin12243@naver.com>
2022-07-02 19:19:14 +0900

    Feat : ex00

--- CPP_Module/CPP_07/ex00/whatever.hpp
+++ CPP_Module/CPP_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 21:10:47 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/07/01 21:11:17 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/07/02 19:16:29 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,31 @@
 
 # include <iostream>
 # include <string>
+
+template<typename T>
+void	swap(T &x, T &y) {
+	if (x == y)
+		return ;
+	T	tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template<typename T>
+T	min(T &x, T &y) {
+	if (x <= y)
+		return x;
+	else
+		return y;
+}
+
+template<typename T>
+T	max(T &x, T &y) {
+	if (x >= y)
+		return x;
+	else
+		return y;
+}
 
 # endif
