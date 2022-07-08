dc2572fc03a983b5e1b089d37920e323d1efb97d
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-07-08 20:41:03 +0900

    Fix : ex00

--- CPP_Module/CPP_08/ex00/easyfind.hpp
+++ CPP_Module/CPP_08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 15:33:53 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/07/06 16:54:40 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/07/08 19:44:35 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 template<typename T>
 typename T::iterator easyfind(T& container, int data) {
 	typename T::iterator	iter;
-
 	// for (iter = container.begin();iter != container.end();iter++) {
 	// 	if (*iter == data)
 	// 		return iter;
